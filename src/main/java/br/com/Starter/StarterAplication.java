7bc584a1903e2bebbe72a0c69299891bc251b5aa
Manrriquez <manrriquez.contato@gmail.com>
2022-11-24 20:50:55 -0300

    projeto criado

---
+++ src/main/java/br/com/Starter/StarterAplication.java
@@ -0,0 +1,13 @@
+package br.com.Starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterAplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterAplication.class, args);
+	}
+
+}
